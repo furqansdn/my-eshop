f6ea83efc5340cc9d57dfaeaaa67950ed6c4a580
Muhammad Furqan Soedono <furqonsoedono71@gmail.com>
2020-12-11 06:55:22 +0700

    Error Handler

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,11 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
-
 connectDB();
-
 const app = express();
 app.use(cors());
 
@@ -16,15 +15,9 @@ app.get('/', (req, res) => {
   res.send('API is running');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
-
+app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 9090;
 app.listen(
   PORT,
